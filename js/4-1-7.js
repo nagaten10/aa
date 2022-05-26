ce664ce316ea5e995fd507dcb38060d200912cef
nagaten10 <98671832+nagaten10@users.noreply.github.com>
2022-05-26 14:24:39 +0900

    フフフ

--- js/4-1-7.js
+++ js/4-1-7.js
@@ -1,33 +1,78 @@
 'use strict';
 
+// //同じ日付で2回目以降ならローディング画面非表示の設定
+
+// // var splash_text = $.cookie('accessdate'); //キーが入っていれば年月日を取得
+// Cookies.set('idol', '1');
+// const idol = Cookies.get('idol');
+// console.log(idol); // => 1
+
+
+// if (idol == '1') {//cookieデータとアクセスした日付を比較↓
+//         $("#splash").css("display", "block");//１回目はローディングを表示
+//         setTimeout(function () {
+//             $("#splash_logo").fadeIn(1000, function () {//1000ミリ秒（1秒）かけてロゴがフェードイン
+//                 setTimeout(function () {
+//             $("#splash_logo").fadeOut(1000);//1000ミリ秒（1秒）かけてロゴがフェードアウト
+//                 }, 1000);//1000ミリ秒（1秒）後に処理を実行
+//         setTimeout(function () {
+//             $("#splash").fadeOut(1000, function () {//1000ミリ秒（1秒）かけて画面がフェードアウト
+//             var myD = new Date();
+//             var myYear = String(myD.getFullYear());
+//             var myMonth = String(myD.getMonth() + 1);
+//             var myDate = String(myD.getDate());
+//             $.cookie('accessdate', myYear + myMonth + myDate); //accessdateキーで年月日を記録
+//         });
+//         }, 1700);//1700ミリ秒（1.7秒）後に処理を実行
+//     });
+// }, 1000);//1000ミリ秒（1秒）後に処理を実行
+//  Cookies.set('idol', '2');
+// const idol = Cookies.get('idol');
+// }else {
+//     $("#splash").css("display", "none");//同日2回目のアクセスでローディング画面非表示
+// }  
+
 //同じ日付で2回目以降ならローディング画面非表示の設定
 
-var splash_text = $.cookie('accessdate'); //キーが入っていれば年月日を取得
-var myD = new Date();//日付データを取得
-var myYear = String(myD.getFullYear());//年
-var myMonth = String(myD.getMonth() + 1);//月
-var myDate = String(myD.getDate());//日
+// var splash_text = $.cookie('accessdate'); //キーが入っていれば年月日を取得
+// var myD = new Date();//日付データを取得
+// var myYear = String(myD.getFullYear());//年
+// var myMonth = String(myD.getMonth() + 1);//月
+// var myDate = String(myD.getDate());//日
+    
+// if (splash_text != myYear + myMonth + myDate) {//cookieデータとアクセスした日付を比較↓
+//         $("#splash").css("display", "block");//１回目はローディングを表示
+//         setTimeout(function () {
+//             $("#splash_logo").fadeIn(1000, function () {//1000ミリ秒（1秒）かけてロゴがフェードイン
+//                 setTimeout(function () {
+//             $("#splash_logo").fadeOut(1000);//1000ミリ秒（1秒）かけてロゴがフェードアウト
+//                 }, 1000);//1000ミリ秒（1秒）後に処理を実行
+//         setTimeout(function () {
+//             $("#splash").fadeOut(1000, function () {//1000ミリ秒（1秒）かけて画面がフェードアウト
+//             var myD = new Date();
+//             var myYear = String(myD.getFullYear());
+//             var myMonth = String(myD.getMonth() + 1);
+//             var myDate = String(myD.getDate());
+//             $.cookie('accessdate', myYear + myMonth + myDate); //accessdateキーで年月日を記録
+//         });
+//         }, 1700);//1700ミリ秒（1.7秒）後に処理を実行
+//     });
+// }, 1000);//1000ミリ秒（1秒）後に処理を実行
+// }else {
+//     $("#splash").css("display", "none");//同日2回目のアクセスでローディング画面非表示
+// } 
 
-console.log(myD);
 
-if (splash_text != myYear + myMonth + myDate) {//cookieデータとアクセスした日付を比較↓
-        $("#splash").css("display", "block");//１回目はローディングを表示
-        setTimeout(function () {
-            $("#splash_logo").fadeIn(1000, function () {//1000ミリ秒（1秒）かけてロゴがフェードイン
-                setTimeout(function () {
-            $("#splash_logo").fadeOut(1000);//1000ミリ秒（1秒）かけてロゴがフェードアウト
-                }, 1000);//1000ミリ秒（1秒）後に処理を実行
-        setTimeout(function () {
-            $("#splash").fadeOut(1000, function () {//1000ミリ秒（1秒）かけて画面がフェードアウト
-            var myD = new Date();
-            var myYear = String(myD.getFullYear());
-            var myMonth = String(myD.getMonth() + 1);
-            var myDate = String(myD.getDate());
-            $.cookie('accessdate', myYear + myMonth + myDate); //accessdateキーで年月日を記録
-        });
-        }, 1700);//1700ミリ秒（1.7秒）後に処理を実行
-    });
-}, 1000);//1000ミリ秒（1秒）後に処理を実行
-}else {
-    $("#splash").css("display", "none");//同日2回目のアクセスでローディング画面非表示
-}  
+
+// sessionStorage.setItem('name','太郎');
+// var sesson = sessionStorage.getItem('name');
+// console.log(sesson);
+
+// if (sesson == '太郎'){
+//     sessionStorage.setItem('name','花子');
+//     var sesson = sessionStorage.getItem('name');
+//     console.log(sesson);
+// } else {
+//     console.log(0);
+// }
+
